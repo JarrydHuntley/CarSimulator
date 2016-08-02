f1b00e07943e1109d8caef3a41928f478941ab80
Jarryd Huntley <jarryd.huntley@gmail.com>
2016-08-01 20:49:53 -0400

    Added in a Audi subclass

---
+++ CarSimulator/Audi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimulator
+{
+    class Audi : Car
+    {
+        public Audi()
+        {
+
+        }
+
+
+    }
+}
