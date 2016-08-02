3070bd6fdb5ca06f4a94b6f475d9fd697469da68
Jarryd Huntley <jarryd.huntley@gmail.com>
2016-08-01 20:17:54 -0400

    Initial commit to our car simulator project.

---
+++ CarSimulator/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimulator
+{
+    class Car
+    {
+        protected double speed;
+        protected double fuelLevel;
+        protected int RPM;
+        protected string engine;
+        protected int doors;
+        protected int windows;
+        protected string color;
+        protected string transmission;
+        protected int HP;
+        protected string drivetrain;
+        protected int seats;
+
+
+
+
+    }
+}
